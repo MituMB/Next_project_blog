e20cca4c0ae9e9db240a1ac5684b9e6a2cb5e8ff
MituMB <mitumariam19@gmail.com>
2023-06-20 14:36:45 +0600

    blog url changed

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import {notFound} from 'next/navigation'
 
 async function getData(id) {
-  const res = await fetch(`http://localhost:3000/api/posts/${id}`,
+  const res = await fetch(`https://next-project-blog-f6096gx6l-mitumb.vercel.app/api/posts/${id}`,
   { cache: 'no-store' }
   )
   if (!res.ok) {
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 async function getData() {
-  const res = await fetch('http://localhost:3000/api/posts',
+  const res = await fetch('https://next-project-blog-f6096gx6l-mitumb.vercel.app/api/posts',
   { cache: 'no-store' }
   )
   if (!res.ok) {
